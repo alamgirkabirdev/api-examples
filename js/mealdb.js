7805d2ee8a21bd87a750fcf3e6f6c46e9fe230ec
Alamgir kabir <alamgirkabir.dev@gmail.com>
2022-10-27 09:41:48 +0600

    5th added

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,18 +1,28 @@
 const searchFood = () => {
 const searchField = document.getElementById('search-field');
 const searchText = searchField.value;
+// clear data
 searchField.value = '';
+if(searchText == ''){
+  return 'please write something';
+}
+else{
+// load data
 const url= `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
 
 // console.log(url);
 fetch(url)
 .then(res => res.json())
 .then(data => displaySearchResult(data.meals));
-
+    }
 }
 
 const displaySearchResult = meals => {
    const searchResult = document.getElementById('search-result');
+   searchResult.textContent = '';
+   if(meals.length == 0){
+    return 'No found this';
+   }
    meals.forEach(meal => {
     // console.log(meal);
     const div = document.createElement('div');
@@ -39,5 +49,18 @@ const loadMealDetail = mealId => {
 }
 
 const displayMealDatail = meal => {
-  console.log(meal.strTags);
+  console.log(meal);
+  const mealDetails = document.getElementById('meal-details');
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerText = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${meal.strMeal}</h5>
+    <p class="card-text">${meal.strInstructions.slice(0,320)}</p>
+    <a href="${meal.strYoutube
+    }" class="btn btn-primary">Go somewhere</a>
+  </div>
+  `;
+mealDetails.appendChild(div);
 }
